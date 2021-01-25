253a7733df2f776c91a35380dbb87b1c1ee5dc22
max.lin <max.lin@collaborate.tw>
2021-01-25 11:18:03 +0800

    排版

--- Spider/Program.cs
+++ Spider/Program.cs
@@ -1,56 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Diagnostics;
 using System.Net;
 using System.Text.RegularExpressions;
 using System.IO;
-using System.Threading.Tasks;
 
 namespace Spider
 {
     class Program
     {
         private const string URL = @"https://www.basketball-reference.com/players/";
-        private const string FILE_PATE = @"C:\Answer\";
+        private const string FILE_PATH = @"C:\Answer\";
         private const string TITLE = "Player,G,PTS,TRB,AST,FG(%),FG3(%),FT(%),eFG(%),PER,WS";
-        private readonly string[] _letter = new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+        private readonly string[] _letters = new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
         private static Program _program = new Program();
 
-
+        /// <summary>
+        /// 抓取URL所有字母球員戰績寫入csv檔。 例:A.csv B.csv....
+        /// </summary>
         static void Main(string[] args)
         {
-            foreach (var i in _program._letter)
-            {
-                TaskFactory factory = new TaskFactory();
-                StringBuilder answer = new StringBuilder();
-                List<Task> taskList = new List<Task>();
-                answer.AppendLine(TITLE);
-                var letterPage = _program.GetHtmlByURL(URL + i + "/"); // 個別字母球員列表頁面
-
-                if (string.IsNullOrEmpty(letterPage)) 
-                    continue;
-
-                var rows = Regex.Split(letterPage, @"<tr ><th");
-                for (int x = 1; x < rows.Length; x++)
-                {
-                    var count = x;
-                    taskList.Add(factory.StartNew(() => _program.GetLetterPlayer(rows[count], i, answer)));
-                }
-
-                if (taskList.Count > 0)
-                {
-                    factory.ContinueWhenAll(taskList.ToArray(), taskArr =>
-                    {
-                        File.WriteAllText(FILE_PATE + i + ".csv", answer.ToString());
-                        Console.WriteLine($"{i.ToUpper()} is OK");
-                    });
-                }
-            }
+            _program.Process();
             Console.ReadKey();
         }
 
+        /// <summary>
+        /// 把每個字母的球員寫入csv
+        /// </summary>
+        private void Process()
+        {
+            foreach (var letter in _program._letters)
+            {
+                StringBuilder answer = new StringBuilder(TITLE);
+                SavePlayersRecord(letter, answer);
+                File.WriteAllText(FILE_PATH + letter + ".csv", answer.ToString()); // 寫入csv檔
+                Console.WriteLine($"{letter.ToUpper()} is OK");
+            }
+        }
+
+        /// <summary>
+        /// 儲存Players數據到answer
+        /// </summary>
+        private static void SavePlayersRecord(string letter, StringBuilder answer)
+        {
+            var letterPage = _program.GetHtmlByURL(URL + letter + "/"); // 個別字母球員列表頁面
+            var rows = Regex.Split(letterPage, @"<tr ><th"); // 擷取每個球員
+            for (int j = 1; j < rows.Length; j++)
+            {
+                _program.GetPlayerContent(rows[j], letter, answer);
+            }
+        }
+
+        /// <summary>
+        /// 取網頁原始碼
+        /// </summary>
         private string GetHtmlByURL(string url)
         {
             var html = string.Empty;
@@ -70,22 +72,16 @@ namespace Spider
             return html;
         }
 
-        private void GetLetterPlayer(string row, string i, StringBuilder answer)
+        /// <summary>
+        /// 擷取LetterPlayer數據
+        /// </summary>
+        private void GetPlayerContent(string row, string i, StringBuilder answer)
         {
-            var ans = string.Empty;
-            var nameSite = Regex.Match(row, @"<a href=""/players/\w/(?<name>[^.]*).html"">(?<baller>[^<]*)<"); //擷取個人數據domain
-            var baller = nameSite.Groups["baller"].Value; // 球員姓名
-            if (string.IsNullOrEmpty(nameSite.Groups["name"].Value) || string.IsNullOrEmpty(nameSite.Groups["baller"].Value)) 
-                return; 
-
-            var ballerRecord = _program.GetHtmlByURL(URL + i + "/" + nameSite.Groups["name"].Value + ".html"); // 字母球員個人資料頁面
-            if (string.IsNullOrEmpty(ballerRecord)) return;
-
-            var values = Regex.Matches(ballerRecord, @"<p>(?<valueOne>[^<]*)</p>\n<p>(?<valueTwo>[^<]*)</p>"); // 擷取個人數據
+            GetNameAndSite(row, out string playerSite, out string playerName);
+            var values = GetRecord(i, playerSite);
 
             string dataOne = "";
             string dataTwo = "";
-            ans += baller;
             foreach (Match value in values)
             {
                 var valueOne = value.Groups["valueOne"].Value;
@@ -94,15 +90,38 @@ namespace Spider
                 dataTwo += "," + valueTwo;
             }
 
+            var ans = playerName;
             if (dataOne.Length > 10) // 判斷個人數據是否為兩行
             {
-                answer.AppendLine(ans + dataOne + "\n" + " " + dataTwo); // 加入兩行個人數據
+                answer.AppendLine(ans + dataOne + "\n" + " " + dataTwo);
             }
             else
             {
                 answer.AppendLine(ans + dataTwo);
             }
-            Console.WriteLine($"{baller} is join");
+            Console.WriteLine($"{playerName} is join");
+        }
+
+        /// <summary>
+        /// 擷取個人數據
+        /// </summary>
+        private MatchCollection GetRecord(string i, string playerSite)
+        {
+            var playerRecord = _program.GetHtmlByURL(URL + i + "/" + playerSite + ".html"); // 字母球員個人資料頁面
+            var values = Regex.Matches(playerRecord, @"<p>(?<valueOne>[^<]*)</p>\n<p>(?<valueTwo>[^<]*)</p>"); // 擷取個人數據
+            return values;
+        }
+
+        /// <summary>
+        /// 提取個人頁面 & 個人名稱
+        /// </summary>
+        private void GetNameAndSite(string row, out string playerSite, out string playerName)
+        {
+            var playerContent = Regex.Match(row, @"<a href=""/players/\w/(?<name>[^.]*).html"">(?<baller>[^<]*)<"); // 擷取球員
+            playerName = playerContent.Groups["baller"].Value;
+            playerSite = playerContent.Groups["name"].Value;
+            if (string.IsNullOrEmpty(playerSite) || string.IsNullOrEmpty(playerName))
+                return;
         }
     }
 }
